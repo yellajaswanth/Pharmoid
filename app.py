7c8c05e659b74f41ea70332b68e96cdf19d83aad
Jaswanth Yella <yellajaswanth@gmail.com>
2022-09-05 15:46:37 -0400

    REST API calls in progress

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from random import sample
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+sample_graph = {
+                    'nodes': [
+                      {'data': {'id':'1', 'name': 'Simvastatin', 'type_': 'drug' } },
+                      {'data': {'id':'2','name':'HMGCR', 'type_': 'gene'} },
+                      {'data': {'id':'3','name':'ITGAL', 'type_': 'gene'} },
+                      {'data': {'id':'4','name':'Cardiovascular Disease', 'type_': 'disease'} }
+                    ],
+                    'edges': [
+                      {'data' : {id: '12', 'source': '1', 'target': '2'}},
+                      {'data' : {id: '13', 'source': '1', 'target': '3'}},
+                      {'data' : {id: '14', 'source': '1', 'target': '4'}}    
+                    ]
+                  }
+
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template("index.html")
+
+@app.route('/api/search/', methods=['GET'])
+def search():
+    return sample_graph['nodes']
+
 
 
 if __name__ == '__main__':
--- static/assets/js/code.js
+++ static/assets/js/code.js
@@ -1,3 +1,39 @@
+
+var sample_graph = {
+                    nodes: [
+                      {'data': {'id':'1', 'name': 'Simvastatin', 'type_': 'drug' } },
+                      {'data': {'id':'2','name':'HMGCR', 'type_': 'gene'} },
+                      {'data': {'id':'3','name':'ITGAL', 'type_': 'gene'} },
+                      {'data': {'id':'4','name':'Cardiovascular Disease', 'type_': 'disease'} }
+                    ],
+                    edges: [
+                      {data : {id: '12', source: '1', target: '2'}},
+                      {data : {id: '13', source: '1', target: '3'}},
+                      {data : {id: '14', source: '1', target: '4'}}    
+                    ]
+                  }
+
+
+$('#search-btn').click(function(){
+  var search_text = $('#search-txt').val()
+  console.log(search_text);
+  // console.log(cy.data())
+  // cy.elements().remove()
+  cy.json({elements: sample_graph});
+  cy.ready(function () {
+      var layout = cy.layout({ name: 'breadthfirst', directed: true, padding: 2 }); 
+      layout.run();
+  });
+
+  $.ajax({
+    url: '/api/search/',
+    success: function(data) {
+       console.log(data)
+      }
+  });
+
+});
+
 var cy = window.cy = cytoscape({
     container: document.getElementById('cy'),
   
@@ -51,20 +87,7 @@ var cy = window.cy = cytoscape({
       }
     ],
   
-    elements: {
-
-      nodes: [
-        {'data': {'id':'1', 'name': 'Simvastatin', 'type_': 'drug' } },
-        {'data': {'id':'2','name':'HMGCR', 'type_': 'gene'} },
-        {'data': {'id':'3','name':'ITGAL', 'type_': 'gene'} },
-        {'data': {'id':'4','name':'Cardiovascular Disease', 'type_': 'disease'} }
-      ],
-      edges: [
-        {data : {id: '12', source: '1', target: '2'}},
-        {data : {id: '13', source: '1', target: '3'}},
-        {data : {id: '14', source: '1', target: '4'}}    
-      ]
-    },
+    // elements: {},
   
     layout: {
       name: 'concentric',
